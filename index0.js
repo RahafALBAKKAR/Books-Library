e589e6e589fa7f635bead7adc9a97cc66152f55b
Rahafalbakkar <>
2021-06-30 19:47:29 +0000

    edit

---
+++ index0.js
@@ -0,0 +1,17 @@
+alert("Welcome to Book library :) ");
+
+alert("Don't show this again");
+
+var preffbook = prompt("Please enter preferred Book?","preffbook");
+
+console.log(preffbook)
+    
+var = userPreference
+if (confirm("Do you want to save Yor preferred Book?") == true) {
+    userPreference = "Data saved successfully!";
+} else {
+    userPreference = "Save Cancelled!";
+}
+Document.write"<h4>preffbook</h4>"
+
+
