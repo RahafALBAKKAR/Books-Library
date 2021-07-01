3bdd7cc113c7cc0a9edbf63d003f5681af54f023
Rahafalbakkar <>
2021-07-01 19:12:54 +0000

    new

--- index0.js
+++ index0.js
@@ -1,12 +1,19 @@
 alert("Hello! In Books library");
 var help =prompt("Do you want any help?")
 
-if (help == "yes"){ var fav =prompt("Could you enter what your preffered Book type ?") 
-var times = prompt( "How many book do toy want download it?") 
-while(times>4){
+if (help === "yes"){ var fav =prompt("Could you enter what your preffered Book type ?") 
+var times = prompt( "How many book do you want to download it?") 
+while(times >= 4){
   alert("Maximum number of books you are allowed to download is 3 books. ")
 } }
-else { prompt("How we can help you?")}
-
-
-
+else { prompt("Thank you")}
+var user;
+if (confirm("Do you the library?") == true) {
+			user = "Welcom!";
+		} 
+console.log(fav)
+console.log(times)
+document.write("<h5>" + fav +"</h5>")
+document.write("<h5>" + times +"</h5>")
+document.write("<h5>" + user +"</h5>")
+console.log(user)
