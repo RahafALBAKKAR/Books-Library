86f6ca98fc16e51e8c4dedbf32336b8e8030fe55
Rahafalbakkar <>
2021-07-01 13:06:18 +0000

    edit

--- index0.js
+++ index0.js
@@ -2,6 +2,17 @@ alert("Welcome to Book library :) ");
 
 var book = prompt("what is your favoite book?" , "book")
 if(book != null)
-{document .write( "<h5>"+ book + "</h5>")}
+{var document.write( "<h5>"+ book + "</h5>")}
 else {alert("Thankyou")}
 
+ var userPreference;
+
+		if (confirm("Do you want to save changes?") == true) {
+			userPreference = "Data saved successfully!";
+		} else {
+			userPreference = "Save Canceled!";
+		}
+
+		document. getElementById("msg").innerHTML = userPreference; 
+
+    document.getElementById("userPreference").style.color = "blue";
