08ed6ceabebaee6951012321f7a6fc5555ccf3a9
Rahafalbakkar <>
2021-07-01 20:31:26 +0000

    edit

--- index0.js
+++ index0.js
@@ -3,13 +3,13 @@ var help =prompt("Do you want any help?")
 
 if (help === "yes"){ var fav =prompt("Could you enter what your preffered Book type ?") 
 var times = prompt( "How many book do you want to download it?") 
-while(times >= 4){
+while(times > 4){
   alert("Maximum number of books you are allowed to download is 3 books. ")
   
 } }
 else { prompt("Thank you")}
 for ( var i = 0 ; i <= times ; i++)
-{document.write("<div>" + "User need 3 books" + "<img src= 'https://www.incimages.com/uploaded_files/image/1920x1080/getty_655998316_2000149920009280219_363765.jpg' ") + "</div>" }
+{document.write innerWidth = 50 ; innerHeight = 50 ("<div >" + "User need 3 books" + "<img  src= 'https://www.incimages.com/uploaded_files/image/1920x1080/getty_655998316_2000149920009280219_363765.jpg' ") + "</div>" }
 var user;
 if (confirm("Do you like the library?") == true) {
 			user = "Welcom!";
