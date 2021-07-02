320384733d1f96afaece7161b64745c6accab80f
Rahafalbakkar <>
2021-07-02 11:37:39 +0000

    edit

--- index0.js
+++ index0.js
@@ -1,33 +1,30 @@
 
 
-var help = prompt("Hello! In Books library","Do you need any help? yes/No!! ")
+var help = prompt("Hello! In Books library","Do you need any help? yes/No! ")
 
 if (help !== "yes" ){
     alert("You are Welcom im the Books library!") }
-else { 
 
   var fav =prompt("Choose one of these books type from your favorite?",
 "Fantasy,Sci-Fi,Mystery")
+
+var times = prompt("How many books do you want to download it?") 
+
+while( times >= 1 && times > 4 ){
+ alert("Sorry.....Maximum number of books you are allowed to download is 3 books.:") 
+break;
 }
 
-var times = prompt( "How many books do you want to download it?") 
-while( times > 4 || times < 0 ){
-  alert( "Sorry.....Maximum number of books you are allowed to download is 3 books. :(" )
-  break;
- }
-   for ( var i = 0 ; i < times ; i++)
-{
-  if (fav == "Fantasy"){
-  document.write("<div >" + "User need these books" + "<img src='https://i.ebayimg.com/images/g/g~MAAOSwgQlepbh3/s-l300.jpg' alt='Fantasy'>" + "</div>")  }
-  if (fav == "Sci-Fi"){
+ for( var i = 0 ; i < times ; i++){
+if (fav == "Fantasy"){
+  document.write("<div >" + "User need these books" + "<img src='https://i.ebayimg.com/images/g/g~MAAOSwgQlepbh3/s-l300.jpg' alt='Fantasy'>" + "</div>") }
+ if (fav == "Sci-Fi"){
   document.write("<div >" + "User need these books" + " <img src='https://images-na.ssl-images-amazon.com/images/I/512U7YktxHL._SX330_BO1,204,203,200_.jpg'>" + "</div>")  }
   if (fav == "Mystery"){
-  document.write("<div >" + "User need these books" + " <img src='https://images-na.ssl-images-amazon.com/images/I/912ziOwTWgL.jpg'>" + "</div>")  }
-  }
- 
+  document.write("<div >" + "User need these books" + " <img src='https://images-na.ssl-images-amazon.com/images/I/912ziOwTWgL.jpg'>" + "</div>") } }
+
+  document.write("<h5>" + fav +"</h5>")
+document.write("<h6>" + times +"</h6>") 
+
 console.log(fav)
 console.log(times)
-document.write("<h5>" + fav +"</h5>")
-document.write("<h6>" + times +"</h6>")
-document.write("<h7>" + user +"</h7>")
-console.log(user)
